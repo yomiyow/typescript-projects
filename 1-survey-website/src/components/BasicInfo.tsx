f97ac148b636b90771b93560bc0f75cf20ab0bbf
Romeo Quinones <romeoquinones4@gmail.com>
2025-04-18 08:21:07 +0800

    feat: add BasicInfo component for user details input

---
+++ 1-survey-website/src/components/BasicInfo.tsx
@@ -0,0 +1,42 @@
+const BasicInfo = () => {
+  return (
+    <fieldset >
+      <h1 className="text-xl mb-2">Basic Details</h1>
+
+      <label htmlFor="name">
+        1. Name
+        <input
+          className="block input input-sm w-full text-base-content"
+          type="text"
+          name="name"
+          id="name"
+          required
+        />
+      </label>
+
+      <label htmlFor="email">
+        2. Email
+        <input
+          className="block input input-sm w-full text-base-content"
+          type="email"
+          name="email"
+          id="email"
+          required
+        />
+      </label>
+
+      <label htmlFor="name">
+        3. Contact No.
+        <input
+          className="block input input-sm w-full text-base-content"
+          type="text"
+          name="name"
+          id="name"
+          required
+        />
+      </label>
+    </fieldset>
+  );
+}
+
+export default BasicInfo;
