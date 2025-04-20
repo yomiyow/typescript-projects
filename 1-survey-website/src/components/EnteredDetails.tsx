1b7a181a8c829aac17b2151fd078b78329aca458
Romeo Quinones <romeoquinones4@gmail.com>
2025-04-20 11:14:16 +0800

    feat: update EnteredDetails component to use formData props for displaying user inputs

--- 1-survey-website/src/components/EnteredDetails.tsx
+++ 1-survey-website/src/components/EnteredDetails.tsx
@@ -5,26 +5,44 @@ interface EnteredDetailsProps {
   onStepChange: () => void;
 };
 
-const EnteredDetails = ({ onStepChange }: EnteredDetailsProps) => {
+const EnteredDetails = ({ formData, onStepChange }: EnteredDetailsProps) => {
+
+  const getAnswer = (n: number) => {
+    const question = formData.questions.find((q) => q.questionId === n);
+
+    if (!question) return "N/A";
+
+    return question.answer;
+  };
+
   return (
     <fieldset className="fieldset flex flex-col gap-3 bg-base-200 border-base-300 w-full max-w-md p-4 rounded">
       <h1 className="text-xl mb-2">Entered Details</h1>
       <p>
         <span className="font-bold">Name: </span>
+        {formData.name}
       </p>
       <p>
         <span className="font-bold">Email: </span>
+        {formData.email}
+      </p>
+      <p>
+        <span className="font-bold">Contact no: </span>
+        {formData.contactNo}
       </p>
 
       <h2>Responses</h2>
       <p>
         <span className="font-bold">Profession: </span>
+        {getAnswer(1)}
       </p>
       <p>
         <span className="font-bold">Interests: </span>
+        {getAnswer(2)}
       </p>
       <p>
         <span className="font-bold">Reference: </span>
+        {getAnswer(3)}
       </p>
 
       <button className="btn btn-small" type="submit" onClick={onStepChange}>Submit</button>
