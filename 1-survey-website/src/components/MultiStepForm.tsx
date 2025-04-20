e0da5ce49a92b318bff5be18477201d920f9ab4d
Romeo Quinones <romeoquinones4@gmail.com>
2025-04-20 16:58:54 +0800

    feat: add pagination

--- 1-survey-website/src/components/MultiStepForm.tsx
+++ 1-survey-website/src/components/MultiStepForm.tsx
@@ -53,22 +53,51 @@ const MultiStepForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="fieldset flex flex-col gap-3 bg-base-200 border-base-300 w-full max-w-md p-4 rounded">
-      {(() => {
-        switch (step) {
-          case 1:
-            return <BasicInfo formData={formData} onFormDataChange={handleChange} onStepChange={handleNextStep} />;
-          case 2:
-            return <Questions formData={formData} onFormDataChange={handleChange} onStepChange={handleNextStep} />;
-          case 3:
-            return <EnteredDetails formData={formData} onStepChange={handleNextStep} />;
-          case 4:
-            return <ThankYou />;
-          default:
-            return null;
-        }
-      })()}
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="fieldset flex flex-col gap-3 bg-base-200 border-base-300 w-full max-w-md p-4 rounded">
+        {(() => {
+          switch (step) {
+            case 1:
+              return <BasicInfo formData={formData} onFormDataChange={handleChange} onStepChange={handleNextStep} />;
+            case 2:
+              return <Questions formData={formData} onFormDataChange={handleChange} onStepChange={handleNextStep} />;
+            case 3:
+              return <EnteredDetails formData={formData} onStepChange={handleNextStep} />;
+            case 4:
+              return <ThankYou />;
+            default:
+              return null;
+          }
+        })()}
+      </form>
+      {step < 4 &&
+        <div className="join">
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label='1'
+            checked={step === 1}
+            onClick={() => setStep(1)}
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label='2'
+            checked={step === 2}
+            onClick={() => setStep(2)}
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label='3'
+            checked={step === 3}
+            onClick={() => setStep(3)}
+          />
+        </div>}
+    </>
   );
 
 };
