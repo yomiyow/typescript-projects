9f2df66fd1934c7b757166fff56ade360bc29643
Romeo Quinones <romeoquinones4@gmail.com>
2025-04-28 13:34:45 +0800

    feat: add message constants for form validation

---
+++ 3-react-hook-form/src/utils/messages.ts
@@ -0,0 +1,8 @@
+const MESSAGE = {
+  REQUIRED: 'This field is required',
+  INVALID_EMAIL: 'Invalid email',
+  MIN_PASSWORD: 'Password must be at least 10 characters',
+  PASSWORDS_MUST_MATCH: 'Passwords must match',
+} as const;
+
+export default MESSAGE;
